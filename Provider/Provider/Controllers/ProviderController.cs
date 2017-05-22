e0e563d90bbba4f144b6f89b117a0ee298e5a7f3
AmirK <amirk_au@hotmail.com>
2017-05-22 20:11:24 +0200

    Change api buy to sell. Prepare post stocks api

--- Provider/Provider/Controllers/ProviderController.cs
+++ Provider/Provider/Controllers/ProviderController.cs
@@ -1,41 +1,25 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Remotion.Linq.Parsing.ExpressionVisitors.Transformation.PredefinedTransformations;
 
 namespace Provider.Controllers
 {
     [Produces("application/json")]
     [Route("api/Provider")]
     public class ProviderController : Controller
-    {
-        //// GET: api/Provider
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        // GET: api/Provider/5
-        //[HttpGet("{id}", Name = "Get")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-        
+    {        
         // POST: api/Provider
         [HttpPost]
-        public void Post([FromBody]string value)
+        [Route("Add")]
+        public void Post(int userId, int stockId, int amount)
         {
+                
         }
         
         // PUT: api/Provider/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+
         }
         
         // DELETE: api/ApiWithActions/5
@@ -45,10 +29,13 @@ namespace Provider.Controllers
         }
 
         [HttpGet]
-        [Route("Buy")]
-        //Localhost example http://localhost:49814/api/provider/buy?id=11&price=10&amount=5322
-        public bool Buy(int id, int amount, int price)
-        {           
+        [Route("Sell")]
+        //Localhost example http://localhost:49814/api/provider/sell?userId=11&stockId=10&amount=5322&price=233
+        public bool Sell(int? userId, int? stockId, int? amount, int? price)
+        {
+            if (userId == null || stockId == null || amount == null || price == null)
+                return false;
+
             return new Random().Next(0, 2) == 1;
         }
     }
