fbde877125429d188e42ed66f5c93faa9f0ff14b
Peter Henriksen <siam.go@hotmail.com>
2017-06-08 23:25:13 +0200

    Bunch

---
+++ Broker/Broker/BrokerService/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BrokerService.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Broker/Broker/Tester/Program.cs
@@ -0,0 +1,24 @@
+﻿using Broker.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Seller seller = new Seller()
+            {
+                Name = "Bob",
+                CustomerId = 1,
+                SellerId = 1,
+                Stocks = new List<Stock>() { new Stock() { Name = "dc", Price = 10, StockId = 2, Amount = 1 } }
+            };
+
+            string json = JsonConvert.SerializeObject(seller);
+        }
+    }
+}
