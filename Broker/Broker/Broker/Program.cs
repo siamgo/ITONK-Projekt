3cb1e106bc3883f96176783713207304ec25032f
Peter Henriksen <siam.go@hotmail.com>
2017-06-08 23:22:39 +0200

    broker fix

--- Broker/Broker/Broker/Program.cs
+++ Broker/Broker/Broker/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
@@ -12,12 +8,13 @@ namespace Broker
     {
         public static void Main(string[] args)
         {
+            
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseUrls("http://*:8080")
                 .Build();
 
             host.Run();
--- Broker/Broker/Broker/Startup.cs
+++ Broker/Broker/Broker/Startup.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using Broker.Models;
 
 namespace Broker
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -24,16 +20,11 @@ namespace Broker
             Configuration = builder.Build();
         }
 
-        public IConfigurationRoot Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
-
-            services.AddDbContext<BrokerContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("BrokerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +33,13 @@ namespace Broker
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseMvc();
-        }
+            app.UseMvc(routes =>
+            {
+                    routes.MapRoute(
+        name: "default",
+        template: "{controller}/{action}/{id?}",
+        defaults: new { controller = "Sellers", action = "Index" });
+            });
+            }
     }
 }
