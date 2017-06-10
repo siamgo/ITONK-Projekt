a1a530e726ab1f33e3472df9c7e5a25ae766e60a
Simon Kæseler <simonkaesler@gmail.com>
2017-06-10 13:56:12 +0200

    All functionality done, couldnt build due to osx

--- Requester/Controllers/RequesterController.cs
+++ Requester/Controllers/RequesterController.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Requester.Model;
+using System.Linq;
 
 namespace Requester
 {
@@ -10,14 +16,76 @@ namespace Requester
         // POST /api/Request
         [HttpPost]
         [Route("Request")]
-        public bool Post([FromQuery] string stockname,[FromQuery]int amount){
+        public bool Post([FromQuery] string stockname,[FromQuery]int amount,[FromQuery]int customerid){
+            
+            //Could check customerID in some sort of DB to get more info
             
             var shareowner = new ShareOwner(){
-                Name = ""
+                Name = "Requester",
+                CustomerId = customerid,
+                Stocks = new List<Stock>() {
+                    new Stock(){
+                        StockId = getStockId(stockname),
+                        Name = stockname,
+                        Amount = amount,
+                        Price = 0 
+                    
+                }}
             };
 
             return true;
         }
 
+        private int getStockId(string stockname){
+            Random rnd = new Random();
+            int id = rnd.Next(10000);
+            return id;
+        }
+
+        private async Task<ShareOwner> payTobinTax(ShareOwner shareowner){
+            
+            using(var client = new HttpClient()){
+                var host = Environment.GetEnvironmentVariable("TOBINTAXER_SERVICE_HOST");
+                var port = Environment.GetEnvironmentVariable("TOBINTAXER_SERVICE_PORT");
+
+                var endpoint = new Uri("http://"+host+":"+port);
+                var response = await client.PostAsync(endpoint+"TobinTaxer/Tax",new StringContent(shareowner.Stocks.FirstOrDefault().Price.ToString()));
+                if(response.IsSuccessStatusCode){
+                    var newPrice = Double.Parse(await response.Content.ReadAsStringAsync()); 
+                    
+                    shareowner.Stocks.FirstOrDefault().Price = newPrice;
+                    return shareowner;
+                }
+                return null;
+            }
+        }
+
+        private async Task<bool> buy(ShareOwner shareowner){
+            if(shareowner != null)
+                {
+                    using (var client = new HttpClient())
+                    {
+                       
+                       var host = Environment.GetEnvironmentVariable("BROKER_SERVICE_HOST");
+                       var port = Environment.GetEnvironmentVariable("BROKER_SERVICE_PORT");
+
+                       var endpoint = new Uri("http://"+host+":"+port);
+
+
+                        //get all shareowners
+                        var jsonShareOwner = JsonConvert.SerializeObject(shareowner);
+                        var content = new StringContent(jsonShareOwner); //Might not work
+                        var allOwners = await client.PostAsync(endpoint + "/Buyers/Buy",content);
+
+                        if(allOwners.IsSuccessStatusCode){
+                            return true;
+                        }
+                    }
+                }
+                return false;
+        }
+
     }
+
+
 }
